65fd9b356219d5650bb4aab08fb388a8dbec581d
Charanraj Tharigonda <charan050505@gmail.com>
2020-03-27 16:18:03 +0000

    Custom Tile

---
+++ appui5/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("appui5.appui5.controller.App", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ appui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"appui5/appui5/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
